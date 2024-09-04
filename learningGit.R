f26ec40d79220bfef97fe2639b65806a04bd8e07
Alex Boyle <abboyle@ksu.edu>
2024-09-04 14:44:11 -0500

    script skeleton

---
+++ learningGit.R
@@ -0,0 +1,114 @@
+fLearningGit <- function(){
+  
+  
+  
+  fChapter1 <- function(){
+    cat(paste0("Hello fChapter1...Running...", Sys.time(), '\n'))
+    #broswer()
+
+    cat(paste0("Goodbye fChapter1...Complete...", Sys.time(), '\n'))
+  } # git and command line
+  
+  fChapter2 <- function(){
+    cat(paste0("Hello fChapter2...Running...", Sys.time(), '\n'))
+    #browser()
+
+    cat(paste0("Goodbye fChapter2...Complete...", Sys.time(), '\n'))
+  } # local repositories
+  
+  fChapter3 <- function(){
+    cat(paste0("Hello fChapter3...Running...", Sys.time(), '\n'))
+    #browser()
+
+    cat(paste0("Goodbye fChapter3...Complete...", Sys.time(), '\n'))
+  } # making a commit
+  
+  fChapter4 <- function(){
+    cat(paste0("Hello fChapter4...Running...", Sys.time(), '\n'))
+    #browser()
+
+    cat(paste0("Goodbye fChapter4...Complete...", Sys.time(), '\n'))
+  } # branches
+  
+  fChapter5 <- function(){
+    cat(paste0("Hello fChapter5...Running...", Sys.time(), '\n'))
+    #browser()
+
+    cat(paste0("Goodbye fChapter5...Complete...", Sys.time(), '\n'))
+  } # merging
+  
+  fChapter6 <- function(){
+    cat(paste0("Hello fChapter6...Running...", Sys.time(), '\n'))
+    #browser()
+    
+    cat(paste0("Goodbye fChapter6...Complete...", Sys.time(), '\n'))
+  } # hosting and authentication
+  
+  fChapter7 <- function(){
+    cat(paste0("Hello fChapter7...Running...", Sys.time(), '\n'))
+    #browser()
+    
+    cat(paste0("Goodbye fChapter7...Complete...", Sys.time(), '\n'))
+  } # creating and pushing to remote repository
+  
+  fChapter8 <- function(){
+    cat(paste0("Hello fChapter8...Running...", Sys.time(), '\n'))
+    #browser()
+    
+    cat(paste0("Goodbye fChapter8...Complete...", Sys.time(), '\n'))
+  } # cloning and fetching
+  
+  fChapter9 <- function(){
+    cat(paste0("Hello fChapter9...Running...", Sys.time(), '\n'))
+    #browser()
+    
+    cat(paste0("Goodbye fChapter9...Complete...", Sys.time(), '\n'))
+  } # three-way merges
+  
+  fChapter10 <- function(){
+    cat(paste0("Hello fChapter10...Running...", Sys.time(), '\n'))
+    #browser()
+    
+    cat(paste0("Goodbye fChapter10...Complete...", Sys.time(), '\n'))
+  } # merge conflicts
+  
+  fChapter11 <- function(){
+    cat(paste0("Hello fChapter11...Running...", Sys.time(), '\n'))
+    #browser()
+    
+    cat(paste0("Goodbye fChapter11...Complete...", Sys.time(), '\n'))
+  } # rebasing
+  
+  fChapter12 <- function(){
+    cat(paste0("Hello fChapter12...Running...", Sys.time(), '\n'))
+    #browser()
+    
+    cat(paste0("Goodbye fChapter12...Complete...", Sys.time(), '\n'))
+  } # pull requests
+  
+
+  fChapter1()
+  fChapter2()
+  fChapter3()
+  fChapter4()
+  fChapter5()
+  fChapter6()
+  fChapter7()
+  fChapter8()
+  fChapter9()
+  fChapter10()
+  fChapter11()
+  fChapter12()
+}
+
+cat("===========================================================\n") 
+cat(paste0( 'learningGit.R Script Running ... ', Sys.time(), "\n.")) 
+#browser() 
+fLearningGit() 
+#browser() 
+cat(paste0( 'learningGit.R Script Complete. ', Sys.time(), "\n.")) 
+cat("===========================================================\n")
+
+
+
+
